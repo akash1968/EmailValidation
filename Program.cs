d2b14f19211c91921380c4cfa317fa5c08f64bfd
Akash <akash.singh670@gmail.com>
2020-10-07 08:34:16 +0530

    [Akash] .Added Email Validation Program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmailValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the emailId : ");
+            string email = Console.ReadLine();
+            RegEx re = new RegEx();
+            re.EmailIdValidation(email);
+
+        }
+    }
+}
